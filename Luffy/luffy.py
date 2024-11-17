6e00edf2b155f069092bacb9f5e032deb04ea9d6
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-11-18 00:59:26 +0900

    luffy run 애니메이션 및 로직 추가

--- Luffy/luffy.py
+++ Luffy/luffy.py
@@ -9,6 +9,7 @@ class Luffy:
     self.x, self.y = 400, 114
     self.face_dir = 1
     self.image_idle = load_image('./res/luffy/luffy_idle.png')
+    self.image_run = load_image('./res/luffy/luffy_run.png')
     self.state_machine = StateMachine(self)
     self.state_machine.start(Idle)
     self.state_machine.set_transitions(
@@ -16,7 +17,11 @@ class Luffy:
         Idle : {state_machine.right_down : Run,
                 state_machine.left_down : Run,
                 state_machine.right_up : Run,
-                state_machine.left_up : Run}
+                state_machine.left_up : Run},
+        Run : {state_machine.right_down : Idle,
+                state_machine.left_down : Idle,
+                state_machine.right_up : Idle,
+                state_machine.left_up : Idle},
       }
     )
   def update(self):
@@ -47,7 +52,7 @@ class Idle:
     luffy.frame = 0
   @staticmethod
   def exit(luffy, e):
-    pass
+    luffy.frame = 0
   @staticmethod
   def do(luffy):
     luffy.frame = (luffy.frame + FRAMES_PER_ACTION_IDLE*ACTION_PER_TIME*game_framework.frame_time) % 4
@@ -59,17 +64,28 @@ class Idle:
 class Run:
   @staticmethod
   def enter(luffy, e):
-    pass
+    if state_machine.right_down(e) or state_machine.left_up(e):  # 오른쪽으로 RUN
+      luffy.dir, luffy.face_dir = 1, 1
+    elif state_machine.left_down(e) or state_machine.right_up(e):  # 왼쪽으로 RUN
+      luffy.dir, luffy.face_dir = -1, -1
 
   @staticmethod
   def exit(luffy, e):
+    luffy.frame = 0
     pass
 
   @staticmethod
   def do(luffy):
-    pass
+    luffy.frame = (luffy.frame + FRAMES_PER_ACTION_IDLE * ACTION_PER_TIME * game_framework.frame_time) % 8
+    luffy.x += luffy.dir * RUN_SPEED_PPS * game_framework.frame_time
 
   @staticmethod
   def draw(luffy):
+    if luffy.face_dir == 1:
+      luffy.image_run.clip_composite_draw(int(luffy.frame) * 100, 0, 100, 180,
+                                           0, '', luffy.x, luffy.y, 120, 120)
+    else:
+      luffy.image_run.clip_composite_draw(int(luffy.frame) * 100, 0, 100, 180,
+                                          0, 'h', luffy.x, luffy.y, 120, 120)
     pass
 
