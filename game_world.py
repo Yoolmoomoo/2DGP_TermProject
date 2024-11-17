7380759d468b9d7b15711882747bbc799493d658
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-11-17 22:56:51 +0900

    state_machine.py 추가

--- game_world.py
+++ game_world.py
@@ -1,6 +1,6 @@
 world = [[] for _ in range(4)]
 
-
+################################# ADD & DEL OBJECT #################################
 
 def add_object(o, depth = 0):
   world[depth].append(o)
@@ -8,6 +8,16 @@ def add_object(o, depth = 0):
 def add_objects(ol, depth = 0):
   world[depth].append += ol
 
+def remove_object(o):
+  for layer in world:
+    if o in layer:
+      layer.remove(o) # list.remove : Remove in "GAME WORLD"
+      #remove_collision_object(o) # Remove in "COLLISION SYSTEM"
+      del o
+      return
+  raise ValueError('Cannot delete non existing object')
+
+################################# UPDATE & RENDER #################################
 def update():
   for layer in world:
     for o in layer:
