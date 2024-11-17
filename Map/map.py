d1f29e23d9484ae9b64a28b2379e35927c11e2bb
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-11-17 18:16:38 +0900

    Map 클래스 추가

---
+++ Map/map.py
@@ -0,0 +1,19 @@
+from pico2d import *
+
+class Map:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        # self.image.draw(400, 30)
+        # self.image.draw(1200, 30)
+      pass
+
+    def get_bb(self):
+        # fill here
+        # return 0, 0, 1600-1, 50
+        pass
+
