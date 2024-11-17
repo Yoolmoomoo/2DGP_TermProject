c936b8c8906af2401733e615a0b7a58f7a6aab42
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-11-17 22:41:29 +0900

    game_framework (pish/pop/change mode 추가)

--- game_framework.py
+++ game_framework.py
@@ -1,5 +1,40 @@
 import time
 
+def change_mode(mode):
+  global stack
+  if (len(stack) > 0):
+    # execute the current mode's finish function
+    stack[-1].finish()
+    # remove the current mode
+    stack.pop()
+  stack.append(mode)
+  mode.init()
+
+
+def push_mode(mode):
+  global stack
+  if (len(stack) > 0):
+    stack[-1].pause()
+  stack.append(mode)
+  mode.init()
+
+
+def pop_mode():
+  global stack
+  if (len(stack) > 0):
+    # execute the current mode's finish function
+    stack[-1].finish()
+    # remove the current mode
+    stack.pop()
+
+  # execute resume function of the previous mode
+  if (len(stack) > 0):
+    stack[-1].resume()
+
+def quit():
+  global running
+  running = False
+
 def run(start_mode):
   global running, stack
   running = True
